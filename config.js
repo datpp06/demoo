3183a0b025e6caae711ad020eb7744fc3fd254ee
unknown <you@example.com>
2021-09-27 17:26:52 +0700

    up load source code

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Tau có điều này muốn hỏi mày, Mày nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Tau biết mày là c hó có phải không phải không ? 😙',
+    desc: 'Mày không trả lời hoặc thoát ra là mày thừa nhận đó nha  :> ',
+    btnYes: 'Có, Tau là c hó',
+    btnNo: 'Không , Tao không phải c hó ',
+    question:'Nói cho Tau biết đi vì sao mày lại thích làm c hó😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì tao thích đó Ahiiiii 😊😊',
+    mess: 'Tau cũng biết mà, Tau cũng thấy mi giống lắm 🥰. Ahihi',
+    messDesc: '💬💬 Nhắn tin cho Tau nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/Đạt Lê'
+}
